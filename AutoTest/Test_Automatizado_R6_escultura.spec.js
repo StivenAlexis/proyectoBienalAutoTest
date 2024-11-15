e6c7ee9e614be2e7ef1e4195483d39032bb45765
Lucas8276 <lucasnahuquin@gmail.com>
2024-11-15 16:18:10 -0300

    Test Reversionados

--- AutoTest/Test_Automatizado_R6_escultura.spec.js
+++ AutoTest/Test_Automatizado_R6_escultura.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://bienal-front-end-viuu.vercel.app/');
-  await page.getByPlaceholder('Buscar...').click();
-  await page.getByPlaceholder('Buscar...').fill('gonzalo');
-  await page.getByRole('link', { name: 'Gonzalo Escultor: Frontvi' }).click();
+  await page.getByPlaceholder('Buscar esculturas, escultores').click();
+  await page.getByPlaceholder('Buscar esculturas, escultores').fill('gonzalo');
+  await page.getByRole('link', { name: 'Gonzaloooo Escultor:' }).click();
 });
